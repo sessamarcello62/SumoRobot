3e497ad4c70dc4428bda99303114639e5202cd06
Marcello <sessamarcello62@gmail.com>
2019-04-12 08:50:02 -0400

    Create MotorControl.ino

---
+++ MotorControl/MotorControl.ino
@@ -0,0 +1,17 @@
+#define LEFTA 5
+#define LEFTB 6
+#define RIGHTA 9
+#define RIGHTB 10
+
+void setup() {
+  pinMode(LEFTA, OUTPUT);
+  pinMode(LEFTB, OUTPUT);
+  pinMode(RIGHTA, OUTPUT);
+  pinMode(RIGHTB, OUTPUT);
+}
+
+void loop() {
+digitalWrite(6,HIGH);
+digitalWrite(5,LOW);
+
+}
